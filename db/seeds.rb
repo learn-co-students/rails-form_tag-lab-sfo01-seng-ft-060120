f1b8ce1a650ec0443f0a0ff69a0cee00a6870e7f
haleighdalke <haleighdalke@gmail.com>
2020-06-24 14:38:15 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = Student.create(first_name: "Haleigh", last_name: "Dalke")
+student2 = Student.create(first_name: "Liza", last_name: "Minelli")
+student3 = Student.create(first_name: "Traci", last_name: "Dalke")
